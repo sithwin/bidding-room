9fb6228a4e6db8dd31559103b56d4e31e72bfc11
Si Thu <sthuwin@myzeller.com>
2026-06-21 09:44:26 +1000

    fix: correct tsconfig extends path and remove committed build artefacts from src

--- packages/shared-types/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROUTING_KEYS = void 0;
-var index_js_1 = require("./events/index.js");
-Object.defineProperty(exports, "ROUTING_KEYS", { enumerable: true, get: function () { return index_js_1.ROUTING_KEYS; } });
